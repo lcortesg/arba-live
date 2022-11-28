453de6866351f1c02ba36e2add35c912b8f12498
Lucas Cortés <lucas.cortes@lanek.cl>
2022-11-28 18:01:59 -0300

    Ahora si

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ Created on Mon Mar 14 16:48:27 2022
 """
 
 
-    
-    
 import cv2
 import streamlit as st
 import mediapipe as mp
@@ -17,17 +15,16 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
-#dsize=(1080,720)
+# dsize=(1280,720)
+st.set_page_config(layout="wide")
+st.title("ABMA Live")
 
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
+run = st.checkbox("Run")
 FRAME_WINDOW = st.image([])
 cap = cv2.VideoCapture(0)
 
 
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while run:
         success, image = cap.read()
         if not success:
@@ -43,16 +40,17 @@ with mp_pose.Pose(
 
         # Draw the pose annotation on the image.
         image.flags.writeable = True
-        #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
+        )
         # Flip the image horizontally for a selfie-view display.
-        #image=cv2.resize(image,dsize)
-        #print(image.shape)
+        # image=cv2.resize(image,dsize)
+        # print(image.shape)
         image = cv2.flip(image, 1)
-        FRAME_WINDOW.image(image)
-        #else:
+        FRAME_WINDOW.image(image, use_column_width=True)
+        # else:
         #    st.write('Stopped')
